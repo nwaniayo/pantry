e58439d08c194539b945d64caea8e67fa6dc345c
nwaniayo <nwaniayomilala@gmail.com>
2024-08-04 21:44:47 +0100

    firestore for client side only

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAZN3B0jrYxOav20YZBedmtdNW8bkwQNxg",
   authDomain: "pantrytracker-19a26.firebaseapp.com",
@@ -17,9 +12,13 @@ const firebaseConfig = {
   measurementId: "G-LK906FYPRN"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+let firestore = null;
+let analytics = null;
 
-export {firestore};
+if (typeof window !== "undefined" && !getApps().length) {
+  const app = initializeApp(firebaseConfig);
+  firestore = getFirestore(app);
+  analytics = getAnalytics(app);
+}
+
+export { firestore, analytics };
